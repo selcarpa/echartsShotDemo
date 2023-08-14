ae6a8e37603f5510d42e651868e3680ca4f40814
selcarpa <selcarpa@gmail.com>
2023-08-14 08:45:17 +0800

    update

--- src/test/java/one/tain/echartsshotdemo/EchartsShotDemoApplicationTests.java
+++ src/test/java/one/tain/echartsshotdemo/EchartsShotDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EchartsShotDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
